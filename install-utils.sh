b73ee923d442b08bd8e8c84f65661bfb3e569779
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-08-28 10:50:58 +0200

    Create install-utils.sh

---
+++ install-utils.sh
@@ -0,0 +1,5 @@
+# Installing utilities
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y zsh neovim ripgrep fzf autojump neofetch htop mc cmake clang wine wine32 cifs-utils

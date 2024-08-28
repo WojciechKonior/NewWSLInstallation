ef44a6cd20cbebec0cf54c9b575cc52ae0be3efa
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-08-28 10:52:45 +0200

    Create configure-zsh.sh

---
+++ configure-zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/grappas/dotfiles $HOME/dotfiles
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+sed -i 's/ZSH_THEME="robbyrussell/ZSH_THEME="powerlevel10k\/powerlevel10k/' $HOME/.zshrc 
+sed -i 's/plugins=(/plugins=(gh colorize nmap rsync ripgrep tmux ssh-agent fzf sudo autojump zsh-syntax-highlighting zsh-autosuggestions /' $HOME/.zshrc
+echo 'export FZF_BASE=/path/to/fzf/install/dir' >> $HOME/.zshrc
